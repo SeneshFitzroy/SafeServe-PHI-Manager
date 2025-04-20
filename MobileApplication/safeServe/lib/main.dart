6735bad55f61081e263960538902197640a84707
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-04-20 14:19:03 +0530

    Updated the main.dart with the new routes

--- MobileApplication/safeServe/lib/main.dart
+++ MobileApplication/safeServe/lib/main.dart
@@ -8,6 +8,12 @@ import 'screens/h800_form/h800_form_screen.dart'; // Import H800FormScreen
 import 'screens/h800_form/h800_form_screen_two.dart'; // Import H800FormScreenTwo
 import 'screens/h800_form/h800_form_screen_three.dart'; // Import H800FormScreenThree
 import 'screens/h800_form/h800_form_screen_four.dart'; // Import H800FormScreenFour
+import 'screens/h800_form/h800_form_screen_five.dart'; // Import H800FormScreenFive
+import 'screens/h800_form/h800_form_screen_six.dart'; // Import H800FormScreenSix
+import 'screens/h800_form/h800_form_screen_seven.dart'; // Import H800FormScreenSeven
+import 'screens/h800_form/h800_form_screen_eight.dart'; // Import H800FormScreenEight
+import 'screens/h800_form/h800_form_screen_nine.dart'; // Import H800FormScreenNine
+import 'screens/h800_form/h800_form_screen_ten.dart'; // Import H800FormScreenTen
 import 'screens/h800_form/h800_form_summary.dart'; // Import H800FormSummary
 import 'screens/h800_form/h800_form_data.dart'; // Import H800FormData
 import 'screens/form_selection/form_selection_screen.dart'; // Import FormSelectionScreen
@@ -87,6 +93,33 @@ class SafeServeApp extends StatelessWidget {
               ModalRoute.of(context)?.settings.arguments as H800FormData?;
           return H800FormScreenFour(formData: formData ?? H800FormData());
         },
+
+        '/h800_form_screen_five': (context) {
+          final formData = ModalRoute.of(context)?.settings.arguments as H800FormData?;
+          return H800FormScreenFive(formData: formData ?? H800FormData());
+        },
+        '/h800_form_screen_six': (context) {
+          final formData = ModalRoute.of(context)?.settings.arguments as H800FormData?;
+          return H800FormScreenSix(formData: formData ?? H800FormData());
+        },
+
+        '/h800_form_screen_seven': (context) {
+          final formData = ModalRoute.of(context)?.settings.arguments as H800FormData?;
+          return H800FormScreenSeven(formData: formData ?? H800FormData());
+        },
+        '/h800_form_screen_eight': (context) {
+          final formData = ModalRoute.of(context)?.settings.arguments as H800FormData?;
+          return H800FormScreenEight(formData: formData ?? H800FormData());
+        },
+        '/h800_form_screen_nine': (context) {
+          final formData = ModalRoute.of(context)?.settings.arguments as H800FormData?;
+          return H800FormScreenNine(formData: formData ?? H800FormData());
+        },
+        '/h800_form_screen_ten': (context) {
+          final formData = ModalRoute.of(context)?.settings.arguments as H800FormData?;
+          return H800FormScreenTen(formData: formData ?? H800FormData());
+        },
+
 
         // Route for H800FormSummary (Summary Screen)
         '/h800_form_summary': (context) {
